5fa1378446514df5fdc5bd6c7242bb29c093e9bf
Erick Vega <evega-12@hotmail.com>
2019-04-22 20:17:21 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           long[] numeros = new long[2] { 0, 1 };
+
+            long resultado = 0;
+            Console.WriteLine("Hola");
+            int i=0;
+            while (i<200)
+            {
+
+                resultado = (numeros[0] + numeros[1]);
+                Console.WriteLine(resultado + "     " + numeros[0] + " " + numeros[1]+" numero "+i);
+                numeros[0] = numeros[1];
+                numeros[1] = resultado;
+              i++;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
